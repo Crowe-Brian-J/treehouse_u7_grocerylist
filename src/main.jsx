6cd4c63431dbd046e7297c23a6e6f6470348edf0
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-10 22:09:50 -0400

    move functions into App

--- src/main.jsx
+++ src/main.jsx
@@ -9,5 +9,37 @@ const Header = () => {
   )
 }
 
+const Item = () => {
+  return (
+    <div className="item">
+      <button className="remove-item" />
+      <span className="item-name">Apples</span>
+      <Counter />
+    </div>
+  )
+}
+
+const Counter = () => {
+  return (
+    <div className="quantity">
+      <span className="qty-label">QTY</span>
+      <button className="increment">+</button>
+      <button className="decrement">-</button>
+      <span className="quantity-amount">15</span>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="grocery-list">
+      <Header />
+
+      {/* Grocery List */}
+      <Item />
+    </div>
+  )
+}
+
 const root = createRoot(document.getElementById('root'))
-root.render(<Header />)
+root.render(<App />)
