59eb89eb20df05ec6a54b17378be64d55de4eb0f
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-10 21:37:42 -0400

    use Header function for jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import { createRoot } from 'react-dom/client'
 
-const desc =
-  'I just learned how to create a React node and render it into the DOM'
-const myTitleId = 'main-title'
-const name = 'Brian'
-
-const element = (
-  //fragment - by wrapping elements in fragment, allows sending multiple tags as single object
-  <>
-    <h1 id={myTitleId}>{name}'s First React Element</h1>
-    <p className="main-desc">{desc}</p>
-  </>
-)
-
-const img = <img src="image.jpg" />
+const Header = () => {
+  return (
+    <header>
+      <h1>Grocery List</h1>
+      <span className="total-items">Items: 1</span>
+    </header>
+  )
+}
 
 const root = createRoot(document.getElementById('root'))
-root.render(element)
+root.render(<Header />)
